cf40bac20410bb7f4112fe4329117098191738f6
Bueong <pcj0308@gmail.com>
2023-01-05 02:41:59 +0900

    12/21 Simple Weather App

---
+++ SimpleWeather/SimpleWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by 박철진 on 2022/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleWeather/SimpleWeather/WeatherViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WeatherViewController.swift
+//  SimpleWeather
+//
+//  Created by 박철진 on 2022/12/21.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    let cities = ["Seoul", "Tokyo", "LA", "Seattle"]
+    let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
+    
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        
+        cityLabel.text = cities.randomElement()
+        
+        let imageName = weathers.randomElement()!
+        weatherImageView.image = UIImage(systemName: imageName)?.withRenderingMode(.alwaysOriginal)
+        
+        let randomTemp = Int.random(in: 10..<30)
+        temperatureLabel.text = "\(randomTemp)°"
+        
+        print("도시, 온도, 날씨 이미지 변경..")
+    }
+}
