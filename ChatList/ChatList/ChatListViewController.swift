777ab1fc6572308503296095246b2d037dc1fc92
Bueong <pcj0308@gmail.com>
2023-01-06 23:19:43 +0900

    ChatList app 커스텀 셀 만들기 & 레이아웃 배치 후 데이터 연동

--- ChatList/ChatList/ChatListViewController.swift
+++ ChatList/ChatList/ChatListViewController.swift
@@ -11,6 +11,8 @@ class ChatListViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    let chatList: [Chat] = Chat.list
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,5 +24,23 @@ class ChatListViewController: UIViewController {
 }
 
 extension ChatListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return chatList.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatListCollectionViewCell", for: indexPath) as? ChatListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let chat = chatList[indexPath.item]
+        cell.configure(chat)
+        return cell
+    }
+}
+
+extension ChatListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 100)
+    }
 }
