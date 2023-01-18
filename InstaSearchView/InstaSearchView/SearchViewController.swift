060c7c5ae48e5aa3ca3dc186da1dbdca37e55d59
Bueong <pcj0308@gmail.com>
2023-01-18 22:50:53 +0900

    datasource 및 delegate 설정

--- InstaSearchView/InstaSearchView/SearchViewController.swift
+++ InstaSearchView/InstaSearchView/SearchViewController.swift
@@ -13,5 +13,27 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        if let flowlayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowlayout.estimatedItemSize = .zero
+        }
     }
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    
 }
