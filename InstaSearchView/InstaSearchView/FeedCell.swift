1bc9941252d1fb81096eb25995d83098c55bbb2b
Bueong <pcj0308@gmail.com>
2023-01-25 21:55:24 +0900

    NewsFeed Page 과제 레이아웃 구성

---
+++ InstaSearchView/InstaSearchView/FeedCell.swift
@@ -0,0 +1,12 @@
+//
+//  FeedCell.swift
+//  InstaSearchView
+//
+//  Created by 박철진 on 2023/01/25.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+}
---
+++ InstaSearchView/InstaSearchView/NewsFeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedViewController.swift
+//  InstaSearchView
+//
+//  Created by 박철진 on 2023/01/25.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
