3485e56f8354258d8f83284d6f041b551df2ab81
Bueong <pcj0308@gmail.com>
2023-01-25 22:14:09 +0900

    NewsFeed Page 과제 커스텀 쉘 작성 및 컨트롤러 연결

--- InstaSearchView/InstaSearchView/FeedCell.swift
+++ InstaSearchView/InstaSearchView/FeedCell.swift
@@ -9,4 +9,14 @@ import UIKit
 
 class FeedCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumbnailImageView.image = nil
+    }
+    
+    func configure(_ imageName: String) {
+        thumbnailImageView.image = UIImage(named: imageName)
+    }
 }
