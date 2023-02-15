4978ed02941e87c988e8bc214625f96de18062ef
Bueong <pcj0308@gmail.com>
2023-02-15 13:54:18 +0900

    AppleFramework app 선택된 객체정보로 모달띄우기 완성 & 사파리 프레임워크 사용해 해당 url 연결해 띄우기

--- AppleFramework_DiffableDatasource/AppleFramework/FrameworkListViewController.swift
+++ AppleFramework_DiffableDatasource/AppleFramework/FrameworkListViewController.swift
@@ -55,5 +55,10 @@ extension FrameworkListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let framework = list[indexPath.item]
         print(">>> selected: \(framework.name)")
+        
+        let storyboard = UIStoryboard(name: "Detail", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
+        vc.framework = framework
+        present(vc, animated: true)
     }
 }
