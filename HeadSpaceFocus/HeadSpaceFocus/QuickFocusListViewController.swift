2bc32adc08057d1c8d194ff741f82a067c718d0e
Bueong <pcj0308@gmail.com>
2023-02-17 19:12:21 +0900

    HeadSpaceFocus App 상세 뷰 기능추가 및 리팩토링

---
+++ HeadSpaceFocus/HeadSpaceFocus/QuickFocusListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuickFocusListViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by 박철진 on 2023/02/17.
+//
+
+import UIKit
+
+class QuickFocusListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
