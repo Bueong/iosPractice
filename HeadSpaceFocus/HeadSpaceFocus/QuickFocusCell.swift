e9c183f31141b63d402ce39c6beacc4d3e264656
Bueong <pcj0308@gmail.com>
2023-02-19 18:58:11 +0900

    QuickFocusCell 생성

---
+++ HeadSpaceFocus/HeadSpaceFocus/QuickFocusCell.swift
@@ -0,0 +1,16 @@
+//
+//  QuickFocusCell.swift
+//  HeadSpaceFocus
+//
+//  Created by 박철진 on 2023/02/19.
+//
+
+import UIKit
+
+class QuickFocusCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ quickFocus: QuickFocus)
+}
