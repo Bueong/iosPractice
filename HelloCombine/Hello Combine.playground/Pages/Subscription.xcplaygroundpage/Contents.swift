007bd3692e7b53dde00c76fb21654eeef64927ca
Bueong <pcj0308@gmail.com>
2023-02-25 20:08:05 +0900

    Subscription 학습

--- HelloCombine/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ HelloCombine/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -4,9 +4,20 @@ import Foundation
 import Combine
 
 
-
+let subject = PassthroughSubject<String, Never>()
 
 // The print() operator prints you all lifecycle events
+let subscription = subject
+    .print("[Debug]")
+    .sink { value in
+    print("Subscriber received value: \(value)")
+}
 
+subject.send("Hello")
+subject.send("Hello again!")
+subject.send("Hellor for the last time")
+//subject.send(completion: .finished)
+subscription.cancel()
+subject.send("Hello..?")
 
 //: [Next](@next)
