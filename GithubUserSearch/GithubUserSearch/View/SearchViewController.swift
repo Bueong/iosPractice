187d5f6b1c12e17d4804f7f47ef7f945fac3296e
Bueong <pcj0308@gmail.com>
2023-03-08 01:55:00 +0900

    CollectionView 생성및 diffable datasource 설정

--- GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
+++ GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -12,9 +12,16 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    typealias Item = SearchResult
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>
+    enum Section {
+        case main
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         embedSearchControl()
+        configureCollectionView()
     }
     
     private func embedSearchControl() {
@@ -26,6 +33,21 @@ class SearchViewController: UIViewController {
         searchController.searchResultsUpdater = self
         searchController.searchBar.delegate = self
         self.navigationItem.searchController = searchController
+    }
+    
+    private func configureCollectionView() {
+        datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else { return nil }
+            
+            cell.user.text = itemIdentifier.login
+            return cell
+        })
+        
+        collectionView.collectionViewLayout = layout()
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        
     }
     //searchController
     
