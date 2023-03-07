2f57a9f5d062062090818c530854e854248ab9f6
Bueong <pcj0308@gmail.com>
2023-03-07 23:54:09 +0900

    searchController 구현

--- GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
+++ GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -14,5 +14,35 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        embedSearchControl()
+    }
+    
+    private func embedSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "E4gleOwl"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
+    }
+    //searchController
+    
+    //collectionView
+    
+    //bind() 검색된 사용자를 collectionView에 업데이트
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search: \(keyword)")
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("button clicked: \(searchBar.text)")
     }
 }
