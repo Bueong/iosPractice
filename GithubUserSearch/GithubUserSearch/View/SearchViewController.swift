7ed4108543f7737a2226fd099357f957eafbe39f
Bueong <pcj0308@gmail.com>
2023-03-10 20:38:31 +0900

    json 디코딩 및 통신 구현

--- GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
+++ GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -10,10 +10,13 @@ import Combine
 
 class SearchViewController: UIViewController {
     
+    @Published private(set) var users: [SearchResult] = []
+    var subscriptions = Set<AnyCancellable>()
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     typealias Item = SearchResult
-    var datasource: UICollectionViewDiffableDataSource<Section, Item>
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
     enum Section {
         case main
     }
@@ -21,7 +24,8 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         embedSearchControl()
-        configureCollectionView()
+        configureCollectionView() 
+        bind()
     }
     
     private func embedSearchControl() {
@@ -47,7 +51,25 @@ class SearchViewController: UIViewController {
     }
     
     private func layout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    private func bind() {
+        $users
+            .receive(on: RunLoop.main)
+            .sink { users in
+                var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+                snapshot.appendSections([.main])
+                snapshot.appendItems(users, toSection: .main)
+                self.datasource.apply(snapshot)
+            }.store(in: &subscriptions)
     }
     //searchController
     
@@ -66,5 +88,31 @@ extension SearchViewController: UISearchResultsUpdating {
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("button clicked: \(searchBar.text)")
+        
+        guard let keyword = searchBar.text, !keyword.isEmpty else { return }
+        let base = "https://api.github.com/"
+        let path = "search/users"
+        let params: [String: String] = ["q": keyword]
+        let header: [String: String] = ["Content-Type": "application/json"]
+        
+        var urlComponents = URLComponents(string: base + path)!
+        let queryItems = params.map { (key: String, value: String) in
+            return URLQueryItem(name: key, value: value)
+        }
+        urlComponents.queryItems = queryItems
+        
+        var request = URLRequest(url: urlComponents.url!)
+        header.forEach { (key: String, value: String) in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: request)
+            .map { $0.data }
+            .decode(type: SearchUserResponse.self, decoder: JSONDecoder())
+            .map { $0.items }
+            .replaceError(with: [])
+            .receive(on: RunLoop.main)
+            .assign(to: \.users, on: self)
+            .store(in: &subscriptions)
     }
 }
