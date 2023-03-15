60952c9bcf6565a3931c551bbd4a8914951a680b
Bueong <pcj0308@gmail.com>
2023-03-15 23:48:51 +0900

    Symbolroller-SwiftUI WIP

--- SwiftUI/SymbolRoller-SwiftUI/SymbolRoller-SwiftUI/SymbolRollerView.swift
+++ SwiftUI/SymbolRoller-SwiftUI/SymbolRoller-SwiftUI/SymbolRollerView.swift
@@ -11,8 +11,35 @@ struct SymbolRollerView: View {
     
     let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
     
+    @State var imageName: String = "moon"
+    
     var body: some View {
-        Text("Hello Symbol Roller")
+        VStack {
+            Image(systemName: imageName)
+                .renderingMode(.template)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            
+            Text(imageName)
+                .font(.system(size: 40, weight: .bold))
+            
+            Button {
+                print("Button Tapped")
+            } label: {
+                HStack {
+                    Image(systemName: "arrow.3.trianglepath")
+                    
+                    VStack {
+                        Text("Reload")
+                        Text("Reload")
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, minHeight: 80)
+            .background(.yellow)
+            .cornerRadius(40)
+        }
     }
 }
 
