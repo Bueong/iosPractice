823584da547a5131e72d091794ceff270e5b21b8
E4gleOwl <pcj0308@gmail.com>
2023-03-19 03:34:55 +0900

    GridItem 주석 추가

--- SwiftUI/AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkListView.swift
+++ SwiftUI/AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkListView.swift
@@ -11,8 +11,10 @@ struct FrameworkListView: View {
     
     @State var list: [AppleFramework] = AppleFramework.list
     
+    //.flexible(사이즈 자유롭게 갯수 유지 .adoptive 사이즈 정해주고 가능한한 갯수 유지 .fixed 고정된 사이즈
     let layout: [GridItem] = [
         GridItem(.flexible()),
+        GridItem(.)
         GridItem(.flexible()),
         GridItem(.flexible()),
     ]
