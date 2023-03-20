c40b245a6f089c3778977ea4f0ee80f69b5a2459
E4gleOwl <pcj0308@gmail.com>
2023-03-21 01:37:12 +0900

    StockRank-SwiftUI Navigation 컨트롤러 추가

--- SwiftUI/AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkListView.swift
+++ SwiftUI/AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkListView.swift
@@ -14,7 +14,6 @@ struct FrameworkListView: View {
     //.flexible(사이즈 자유롭게 갯수 유지 .adoptive 사이즈 정해주고 가능한한 갯수 유지 .fixed 고정된 사이즈
     let layout: [GridItem] = [
         GridItem(.flexible()),
-        GridItem(.)
         GridItem(.flexible()),
         GridItem(.flexible()),
     ]
